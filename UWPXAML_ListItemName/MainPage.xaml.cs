8445784703d4387d8d4efa5bf032775e8c8cf944
Guy Barker (CELA) <guybark@microsoft.com>
2020-10-08 11:55:48 -0700

    Add project files.

---
+++ UWPXAML_ListItemName/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPXAML_ListItemName
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Populate the list with some test data.
+            List<Bird> items = new List<Bird>();
+            items.Add(new Bird() { 
+                Name = "House Sparrow", 
+                Habitat = "Cities, suburbs, farms", 
+                Voice = "Repeated series of chirrup sounds" });
+            items.Add(new Bird() { 
+                Name = "Golden-crowned Sparrow", 
+                Habitat = "Brushy places, including neighborhoods", 
+                Voice = "Series of long, raspy, whistled notes" });
+            items.Add(new Bird() { 
+                Name = "Song Sparrow", 
+                Habitat = "Found throughout Puget Sound Region, up to mountain passes", 
+                Voice = "Song begins with several clear notes, followed by lower note, jumbled trill" });
+            BirdList.ItemsSource = items;
+        }
+    }
+
+    public class Bird
+    {
+        public string Name { get; set; }
+        public string Habitat { get; set; }
+        public string Voice { get; set; }
+    }
+}
